fba3690e041bfdc5c16d1b15db3c8490ff9e9c03
minishrink <mathur_akanksha@hotmail.com>
2018-12-21 23:08:48 +0000

    Add Dict class
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

---
+++ dict.hpp
@@ -0,0 +1,54 @@
+
+#include "hashtbl.hpp"
+
+#define Hash Hashtbl<K, V>
+
+template<typename K, typename V>
+class Dict  {
+  // identity to 0
+  int id(K key)  {
+    return 0;
+  }
+
+  private:
+    static const int len;
+    Hash tbl;
+
+  public:
+    Dict();
+    Dict (const int n)  {
+      const int len = n;
+      // initialise underyling hashtbl
+      Hash tbl = new Hash(id, len);
+    }
+
+    V search(K key)  {
+      try {
+        return tbl.get(key);
+      }
+      catch (const std::out_of_range& oor) {
+        std::cerr << "Out of Range error: " << oor.what() << '\n';
+      }
+      catch (...) {
+        std::cerr << "Unexpected error occurred\n";
+      }
+      // FIXME best way to handle this?
+      throw 0;
+    }
+
+    void insert(K key, V value) {
+      int i = tbl.index(key);
+      tbl.set(i, value);
+    }
+
+    void remove(K key)  {
+      tbl.remove(key);
+    }
+
+    void del () {
+      // hashtbl destructor handles internal stuff
+      tbl.del();
+      delete &this;
+    }
+
+};
